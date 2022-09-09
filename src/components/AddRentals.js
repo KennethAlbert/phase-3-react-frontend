03f195933fec649cc03446a50c21a4d17d6291c9
Kenneth Albert <gichuka@gmail.com>
2022-09-10 02:07:32 +0300

    updates

--- src/components/AddRentals.js
+++ src/components/AddRentals.js
@@ -64,7 +64,7 @@ function AddRentals({handleAdd}) {
     <input type="text" name="location" onChange={onchange}/><br/><br/>
     <label htmlFor="price" value="price" >Price:</label>
     <input type="interger"  name="price" onChange={onchange}/><br/>
-    <input type="submit"  style={{marginTop:"2rem",padding:"15px",border:"none",background:"pink"}}   value="Add Rentals"/>
+    <input type="submit" className="delete" style={{marginTop:"2rem",padding:"15px",border:"none"}}   value="Add Rentals"/>
   </form>
   
   </div>
